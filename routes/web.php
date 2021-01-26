617938ec91343efa8a66b9226ff14dba96e7b899
Md Murad Hosen <muradbd.info@gmail.com>
2021-01-26 22:35:12 +0600

    Create Shop, Cart And Checkout Page-> Part2

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,3 +21,9 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/',App\Http\Livewire\HomeComponent::class);
+
+Route::get('/shop',App\Http\Livewire\ShopComponent::class);
+
+Route::get('/cart',App\Http\Livewire\CartComponent::class);
+
+Route::get('/checkout',App\Http\Livewire\CheckoutComponent::class);
